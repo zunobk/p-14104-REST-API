3b9ef21b9ecaf5bb16a70c17069424f81250803e
zunobk <junho4560@naver.com>
2025-12-05 15:24:54 +0900

    RsData는 RsData<Object>와 같다. 하지만 지양하는게 좋다. 댓글 삭제 메서드의 응답 타입에 제너릭 적용

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,6 +1,4 @@
 package com.back.global.rsData;
 
-import com.back.domain.post.postComment.dto.PostCommentDto;
-
-public record RsData(String resultCode, String msg, PostCommentDto data) {
+public record RsData<T>(String resultCode, String msg, T data) {
 }
