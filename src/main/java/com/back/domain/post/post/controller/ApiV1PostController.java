ab5c1d6918c292a08d7d699927fa3bc11441cd81
zunobk <junho4560@naver.com>
2025-12-05 10:42:23 +0900

    클래스 레벨에 @RequestMapping 추가하여 공통 접두어 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,19 +3,19 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody
+    @GetMapping
     public List<Post> getItems() {
         List<Post> items = postService.findAll();
 
