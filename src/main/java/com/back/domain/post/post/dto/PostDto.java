d60c6d994a960dc54c080dfcd0593d64f3e9d716
zunobk <junho4560@naver.com>
2025-12-05 12:30:52 +0900

    프론트 개발자의 요구 : createdDate → createDate, subject → title, body → content

--- src/main/java/com/back/domain/post/post/dto/PostDto.java
+++ src/main/java/com/back/domain/post/post/dto/PostDto.java
@@ -6,10 +6,10 @@ import java.time.LocalDateTime;
 
 public record PostDto(
         int id,
-        LocalDateTime createdDate,
-        LocalDateTime modifiedDate,
-        String subject,
-        String body
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String title,
+        String content
 ) {
     public PostDto(Post post) {
         this(
