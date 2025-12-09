9f1e0e15297bc0e95a6e26edea7a7cf860ca544b
zunobk <junho4560@naver.com>
2025-12-09 12:08:31 +0900

    댓글 작성 테스트케이스 추가

--- src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
+++ src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
@@ -132,4 +132,37 @@ public class ApiV1PostCommentControllerTest {
                 .andExpect(jsonPath("$.resultCode").value("200-1"))
                 .andExpect(jsonPath("$.msg").value("%d번 댓글이 수정되었습니다.".formatted(id)));
     }
+
+    @Test
+    @DisplayName("댓글 작성")
+    void t5() throws Exception {
+        int postId = 1;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts/%d/comments".formatted(postId))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "content": "내용"
+                                        }
+                                        """)
+                )
+                .andDo(print());
+
+        Post post = postService.findById(postId).get();
+
+        PostComment postComment = post.getComments().getLast();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostCommentController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 댓글이 작성되었습니다.".formatted(postComment.getId())))
+                .andExpect(jsonPath("$.data.id").value(postComment.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(postComment.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(postComment.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.content").value("내용"));
+    }
 }
