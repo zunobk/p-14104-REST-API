31b04bff25533750bc625ed6d6768301fb670872
zunobk <junho4560@naver.com>
2025-12-09 12:32:58 +0900

    SpringDocConfig 추가

---
+++ src/main/java/com/back/global/springDoc/SpringDocConfig.java
@@ -0,0 +1,9 @@
+package com.back.global.springDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SpringDocConfig {
+}
