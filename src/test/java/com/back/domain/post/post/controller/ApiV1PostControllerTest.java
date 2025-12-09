b4c184fac44b42f9df19aae25a76836096cea2a9
zunobk <junho4560@naver.com>
2025-12-09 11:12:10 +0900

    내용없이 글 작성 시도 == 400

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -88,6 +88,33 @@ public class ApiV1PostControllerTest {
                         """.stripIndent().trim()));
     }
 
+    @Test
+    @DisplayName("글 쓰기, without content")
+    void t8() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": ""
+                                        }
+                                        """)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        content-NotBlank-must not be blank
+                        content-Size-size must be between 2 and 5000
+                        """.stripIndent().trim()));
+    }
+
 
     @Test
     @DisplayName("글 수정")
