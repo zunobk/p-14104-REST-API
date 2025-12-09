08c6ca307e72dfd37c2c4a54c61e6c44ca89d631
zunobk <junho4560@naver.com>
2025-12-09 12:33:22 +0900

    @Info 어노테이션을 추가해서 문서의 제목/버전/설명 추가

--- src/main/java/com/back/global/springDoc/SpringDocConfig.java
+++ src/main/java/com/back/global/springDoc/SpringDocConfig.java
@@ -1,9 +1,10 @@
 package com.back.global.springDoc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(info = @Info(title = "API 서버", version = "beta", description = "API 서버 문서입니다."))
 public class SpringDocConfig {
 }
