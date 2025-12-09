631cf7110703715caa63f4f6d35557edc4182886
zunobk <junho4560@naver.com>
2025-12-09 10:42:46 +0900

    정답코드 B : ResponseEntity 사용하면 @RestControllerAdvice일 필요가 없으니 @ControllerAdvice 로 변경

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -2,23 +2,25 @@ package com.back.global.globalExceptionHandler;
 
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@RestControllerAdvice
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(NOT_FOUND)
-    public RsData<Void> handle() {
-        return new RsData<>(
-                "404-1",
-                "해당 데이터가 존재하지 않습니다."
+    public ResponseEntity<RsData<Void>> handle() {
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "404-1",
+                        "해당 데이터가 존재하지 않습니다."
+                ),
+                NOT_FOUND
         );
     }
 }
