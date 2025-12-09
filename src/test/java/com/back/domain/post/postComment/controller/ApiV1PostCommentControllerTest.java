a1f2d6413a25a912fdb61bcac556b670c9b99284
zunobk <junho4560@naver.com>
2025-12-09 11:25:14 +0900

    ApiV1PostCommentControllerTest에 본격적인 테스트를 위한 준비작업

--- src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
+++ src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
@@ -1,4 +1,20 @@
 package com.back.domain.post.postComment.controller;
 
+import com.back.domain.post.post.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ApiV1PostCommentControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private PostService postService;
 }
